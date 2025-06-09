c4b9f289f1e8be957430624496029a12f3fd2f5e
unknown <mattm@MAIN>
2025-06-09 13:53:36 -0500

    Updated app.py app.run part

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def tank_levels():
     return render_template("index.html", headers=headers, rows=rows, title="Tank Level Readings")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
